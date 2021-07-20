b5c1abb24d068570f3e9990bd518901981aa1deb
manArr-dev <84402072+manArr-dev@users.noreply.github.com>
2021-07-20 13:56:20 -0500

    second commit

--- src/main/java/org/Foo/Bar/Services/UserService.java
+++ src/main/java/org/Foo/Bar/Services/UserService.java
@@ -4,4 +4,7 @@ import org.Foo.Bar.Entities.User;
 
 public interface UserService {
   void persistUser(User user);
+  List<User> getAllUsers();
+  void addUser(User user);
+  User getUserById(Integer id);
 }
